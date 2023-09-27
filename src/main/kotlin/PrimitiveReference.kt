ca67880143f13d92b7fcfe15e1d8d91016aed851
Shu Wang <shugewang@hotmail.co.uk>
2023-09-27 21:31:06 +0100

    Add primitive and reference type example

---
+++ src/main/kotlin/PrimitiveReference.kt
@@ -0,0 +1,18 @@
+fun main() {
+    var person1Age: Int = 25
+    val person2Age: Int = person1Age
+    person1Age = 26
+    println("Person1 Age: $person1Age")
+    println("Person2 Age: $person2Age")
+
+    // Custom Reference Type (Class)
+    class Person(val name: String, var age: Int)
+
+    var person1 = Person("Bob", 25)
+    var person2 = person1 // Reference to the same object
+
+    person1.age = 26 // Modifying the object
+
+    println("Person 1: ${person1.name}, ${person1.age}")
+    println("Person 2: ${person2.name}, ${person2.age}")
+}
