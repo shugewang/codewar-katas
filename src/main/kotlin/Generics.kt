0068ff527bfd9937d4f85b1005b0ea17a0f8cc5b
Shu Wang <shugewang@hotmail.co.uk>
2023-09-29 13:17:07 +0100

    Add to generics example

--- src/main/kotlin/Generics.kt
+++ src/main/kotlin/Generics.kt
@@ -1,18 +1,24 @@
-class Box<T>(val value: T)
+class Box<T,A>(val value: T, val strValue: A) {
+    fun returnValue(): Pair<T, A> {
+        return Pair(value, strValue)
+    }
+}
 fun <T> printGeneric(input: T) {
     println("Input: $input")
+    return
 }
 fun main() {
     val intVar = 1
     val stringVar = "Generic"
-    val boxOfInt = Box(intVar)
-    val boxOfString = Box(stringVar)
+    val boxOfInt = Box(intVar, stringVar)
+//    val boxOfString = Box(stringVar)
 
     println(boxOfInt.value)
-    println(boxOfString.value)
+//    println(boxOfString.value)
 
     printGeneric(intVar)
     printGeneric(stringVar)
+    println(boxOfInt.returnValue())
 }
 
 // are generic functions type safe?
